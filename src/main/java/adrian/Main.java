e84152c028dfb6750434fcdd23aab08f51b62704
Onii-chan7u7 <adr793@gmail.com>
2023-05-16 22:40:48 +0200

    Prueba hilos + comandos

--- src/main/java/adrian/Main.java
+++ src/main/java/adrian/Main.java
@@ -11,44 +11,83 @@ import reactor.core.publisher.Mono;
 public class Main {
 
     public static void main(String[] args) {
-        DiscordClient client = DiscordClient.create("");
+        DiscordClient client = DiscordClient.create("MTEwNTg4NjI0MjYyMjY4MTE5OA.GO2bxU.mIhVCBNUKnm0eE6uIAF5JzsYUwRaHYqlb3SLkE");
 
         //Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> Mono.empty());
 
-        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> {
-            Mono<Void> printOnLogin = gateway.on(ReadyEvent.class, event ->
-                Mono.fromRunnable(() -> {
-                  final User self = event.getSelf();
-                  System.out.printf("Iniciado como %s#%s%n", self.getUsername(), self.getDiscriminator());
-            })).then();
-          
-            Mono<Void> handlePingCommand = gateway.on(MessageCreateEvent.class, event -> {
-                Message message = event.getMessage();
-            
-                if (message.getContent().equalsIgnoreCase("!ping")) {
-                    return message.getChannel()
-                        .flatMap(channel -> channel.createMessage("pong!"));
-                }
-            
-                return Mono.empty();
-            }).then();
+        new Thread(() -> {
+            Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) -> {
+                Mono<Void> printOnLogin = gateway.on(ReadyEvent.class, event ->
+                    Mono.fromRunnable(() -> {
+                      final User self = event.getSelf();
+                      System.out.printf("Iniciado como %s#%s%n", self.getUsername(), self.getDiscriminator());
+                })).then();
+              
+                Mono<Void> handlePingCommand = gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+                
+                    if (message.getContent().equalsIgnoreCase("!ping")) {
+                        return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("pong!"));
+                    }
+                
+                    return Mono.empty();
+                }).then();
+    
+                Mono<Void> holaMundo = gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+                
+                    if (message.getContent().equalsIgnoreCase("!hola")) {
+                        return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("Hola mundo!"));
+                    }
+                
+                    return Mono.empty();
+                }).then();
+    
+                // combine them!
+                return printOnLogin.and(handlePingCommand).and(holaMundo);
+            });
+                
+            login.block();
+        }).start();
 
-            Mono<Void> holaMundo = gateway.on(MessageCreateEvent.class, event -> {
-                Message message = event.getMessage();
-            
-                if (message.getContent().equalsIgnoreCase("!hola")) {
-                    return message.getChannel()
-                        .flatMap(channel -> channel.createMessage("Hola mundo!"));
-                }
-            
-                return Mono.empty();
-            }).then();
-
-            // combine them!
-            return printOnLogin.and(handlePingCommand).and(holaMundo);
-        });
-            
-        login.block();
+        new Thread(() -> {
+            Mono<Void> comandos = client.withGateway((GatewayDiscordClient gateway) -> {
+                Mono<Void> printOnLogin = gateway.on(ReadyEvent.class, event ->
+                    Mono.fromRunnable(() -> {
+                      final User self = event.getSelf();
+                      System.out.printf("Iniciando comandos");
+                })).then();
+              
+                Mono<Void> handlePingCommand = gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+                
+                    if (message.getContent().equalsIgnoreCase("!ping2")) {
+                        return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("pong2!"));
+                    }
+                
+                    return Mono.empty();
+                }).then();
+    
+                Mono<Void> holaMundo = gateway.on(MessageCreateEvent.class, event -> {
+                    Message message = event.getMessage();
+                
+                    if (message.getContent().equalsIgnoreCase("!adios")) {
+                        return message.getChannel()
+                            .flatMap(channel -> channel.createMessage("Adios mundo!"));
+                    }
+                
+                    return Mono.empty();
+                }).then();
+    
+                // combine them!
+                return printOnLogin.and(handlePingCommand).and(holaMundo);
+            });
+                
+            comandos.block();
+        }).start();
         
     }
 
